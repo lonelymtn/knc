6fcf73f30b61485edf7399ae4e0832e9562c3cd0
Ryan Corder <ryanc@greengrey.org>
2012-02-08 16:12:47 -0800

    filled out more documentation. added socket close statements to AE handle on_eof/on_error callbacks.

--- knc.pl
+++ knc.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/env perl
-# Time-stamp: <2012-02-08 12:55:03 (ryanc)>
+# Time-stamp: <2012-02-08 16:09:06 (ryanc)>
 #
 # Author: Ryan Corder <ryanc@greengrey.org>
 # Description: knc.pl - Eventual OpenBSD netcat clone with Kerberos support
@@ -16,41 +16,14 @@ use English qw(-no_match_vars);
 use Getopt::Std;
 use IO::Socket;
 
-=head1 NAME
-
-knc.pl - Eventual OpenBSD netcat clone with Kerberos support.
-
-=head1 VERSION
-
-This README refers to knc.pl version 0.001.
-
-=head1 SYNOPSIS
-
-    Simple server:
-
-        knc.pl -l 127.0.0.1 12345
-
-    Simple client:
-
-        knc.pl 127.0.0.1 12345
-
-    Kerberized server:
-
-        knc.pl -l -K -N example -T /opt/etc/krb5.keytab 127.0.0.1 12345
-
-    Kerberized client:
-
-        kinit -f (You will need a TGT from your KDC first)
-        knc.pl -K -N example 127.0.0.1 12345
-=cut
-
 $Getopt::Std::STANDARD_HELP_VERSION = 'true';
 
 my %opts;
 getopts( 'CkKN:lT:v', \%opts );
 
+# Exit early if certain required things are missing
 die HELP_MESSAGE() if ( !( $ARGV[0] && $ARGV[1] ) );
-die HELP_MESSAGE() if ( !( $opts{'l'} && $opts{'k'} ) );
+die HELP_MESSAGE() if ( $opts{'k'} && ( !$opts{'l'} ) );
 
 my $host   = $ARGV[0];             # IO::Socket::INET does host/ip validaiton
 my $port   = $ARGV[1];             # IO::Socket::INET does port validation
@@ -111,28 +84,8 @@ my $socket_handle = setup_socket_handle();
 # "Main Loop"
 $ready->recv;
 
-exit 0;
-
-# Clean up after we finish
-END {
-    if ( $opts{'K'} ) {
-        Authen::Krb5::free_context()
-            or warn "KRB5 free context fail: $OS_ERROR\n";
-    }
-
-    if ($socket) {
-        shutdown $socket, 2 or warn "Shutdown socket fail: $OS_ERROR\n";
-        close $socket or warn "Close socket filehandle fail: $OS_ERROR\n";
-    }
-
-    if ($socket_handle) {
-        $socket_handle->destroy
-            or warn "Destroy socket handle fail: $OS_ERROR\n";
-    }
-    if ($stdin_handle) {
-        $stdin_handle->destroy
-            or warn "Destroy stdin handle fail: $OS_ERROR\n";
-    }
+if ( $opts{'K'} ) {
+    Authen::Krb5::free_context();
 }
 
 ##
@@ -169,6 +122,8 @@ sub setup_socket_handle {
         no_delay => 1,
         on_eof   => sub {
             my ($hdl) = @_;
+            shutdown $socket, 2;
+            close $socket or AE::log error => "Shutdown socket failed.\n";
             undef $hdl;
             $ready->send;
         },
@@ -176,6 +131,8 @@ sub setup_socket_handle {
             my ( $hdl, $fatal, $msg ) = @_;
             AE::log error => "$msg\n";
             undef $hdl;
+            shutdown $socket, 2;
+            close $socket or AE::log error => "Shutdown socket failed.\n";
             $ready->send;
         },
         on_read => sub {
@@ -202,12 +159,16 @@ sub setup_stdin_handle {
         on_eof   => sub {
             my ($hdl) = @_;
             $hdl->destroy;
+            shutdown $socket, 2;
+            close $socket or AE::log error => "Shutdown socket failed.\n";
             $ready->send;
         },
         on_error => sub {
             my ( $hdl, $fatal, $msg ) = @_;
             AE::log error => "$msg\n";
             $hdl->destroy;
+            shutdown $socket, 2;
+            close $socket or AE::log error => "Shutdown socket failed.\n";
             $ready->send;
         },
         on_read => sub {
@@ -227,46 +188,97 @@ sub setup_stdin_handle {
     return $l_stdin_handle;
 }
 
-=head1 DIAGNOSTICS
-
-Run the program with the '--help' swith for alist of all available options.
-
-=cut
-
 sub HELP_MESSAGE {
-    print "\n"
-        . "Usage: $0 [-CkKlv] [-N 'service name'] [-T /path/to/krb5.keytab]\n"
-        . "       <hostname> <port>\n" . "\n"
-        . "Examples:\n"
-        . "    See the README or run this file through perldoc to see\n"
-        . "    examples\n" . "\n"
-        . "Syntax:\n"
-        . "    -C    Send CRLF as line-ending.\n" . "\n"
-        . "    -k    Forces $0 to stay listening for another connection after\n"
-        . "          its current onneciton is completed.  It is an error to\n"
-        . "          use this options without the -l option.\n" . "\n"
-        . "    -K    Attempt to perform Kerberos authentication.\n" . "\n"
-        . "    -l    Used to specify that $0 should listen for an incoming\n"
-        . "          connextion rather than initiate a connection to a\n"
-        . "          remote host.\n" . "\n"
-        . "    -v    Have $0 give more verbose output.\n" . "\n"
-        . "    -N <service name>\n"
-        . "          Specifies which Kerberized service to attempt to\n"
-        . "          authenticate to.  A matching Service Principal Name\n"
-        . "          must exist in your REALM.  The default is 'example'.\n"
-        . "\n"
-        . "    -T <keytab>\n"
-        . "          Specifies an alternative location for your keytab.\n"
-        . "          The default is /etc/krb5.keytab.\n" . "\n";
+    print << 'END_HELP';
+
+Usage: knc.pl [-CkKlv] [-N 'service name'] [-T /path/to/krb5.keytab]
+       <hostname> <port>
+
+Examples:
+    See the README or run this file through perldoc to see examples.
+
+Syntax:
+    -C    Send CRLF as line-ending.
+
+    -k    Forces knc.pl to stay listening for another connection after its
+          current conneciton is completed.  It is an error to use this
+          options without the -l option.
+
+    -K    Attempt to perform Kerberos authentication.
+
+    -l    Used to specify that knc.pl should listen for an incoming connection
+          rather than initiate a connection to a remote host.
+
+    -v    Have knc.pl give more verbose output.
+
+    -N <service name>
+          Specifies which Kerberized service to attempt to authenticate to.  A
+          matching Service Principal Name must exist in your REALM.  The
+          default is 'example'
+
+    -T <keytab>
+          Specifies an alternative location for your keytab.  The default is
+          /etc/krb5.keytab.
+
+END_HELP
 
     return 1;
 }
+
+__END__
+
+=head1 NAME
+
+knc.pl - Eventual OpenBSD netcat clone with Kerberos support.
+
+=head1 VERSION
+
+This README refers to knc.pl version 0.001.
+
+=head1 USAGE
+
+Simple server:
+
+    knc.pl -l 127.0.0.1 12345
+
+Simple client:
+
+    knc.pl 127.0.0.1 12345
+
+Kerberized server:
+
+    knc.pl -l -K -N example -T /opt/etc/krb5.keytab 127.0.0.1 12345
+
+Kerberized client:
+
+    kinit -f (You will need a TGT from your KDC first)
+    knc.pl -K -N example 127.0.0.1 12345
+
+=head1 REQUIRED ARGUMENTS
+
+The Hostname/IP address and Port of the host you either want to connect to
+or listen on are required.
+
+=head1 OPTIONS
+
+Run the program with the '--help' swith for alist of all available options.
+
+=head1 DESCRIPTION
+
+knc.pl aims to be a full clone of netcat that comes with OpenBSD.
+Additionally, knc.pl supports Kerberos authentication in both client and
+server modes.
+
+As of this release, features are few.
+
+For more information on OpenBSD's netcat, please see the current manual
+at L<http://www.openbsd.org/cgi-bin/man.cgi?query=nc>
 
 =head1 AUTHOR
 
 Ryan Corder, C<ryanc at greengrey.org>
 
-=head1 KNOWN BUGS
+=head1 BUGS AND LIMITATIONS
 
 Many of the features/switches from OpenBSD's netcat are not yet implemented.
 
@@ -275,17 +287,27 @@ altogether.
 
 =head1 ACKNOWLEDGEMENTS
 
-The OpenBSD project: L<http://openbsd.org>
+The OpenBSD project: L<http://www.openbsd.org>
 
 Author(s) of the AnyEvent framework: L<https://metacpan.org/release/AnyEvent>
 
 Author(s) of Authen::Krb5: L<https://metacpan.org/module/Authen::Krb5>
 
-=head1 COPYRIGHT & LICENSE
+=head1 LICENSE AND COPYRIGHT
 
 Copyright 2012 Ryan corder, All Rights Reserved.
 
 This program is free software; you can redistribute it and/or modify it under
 the same terms as Perl itself.
+
+=head1 DIAGNOSTICS
+
+=head1 CONFIGURATION
+
+=head1 DEPENDENCIES
+
+=head1 INCOMPATIBILITIES
+
+=head1 EXIT STATUS
 
 =cut
