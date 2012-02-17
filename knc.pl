a6a9029e42da590925cc465eb5eefd941ccc4dda
Ryan Corder <ryanc@greengrey.org>
2012-02-16 16:58:40 -0800

    Kerberos mode now encrypts messages between peers. Now using AE::Log for all logging/info/debug/etc.

--- knc.pl
+++ knc.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/env perl
-# Time-stamp: <2012-02-10 00:02:16 (ryanc)>
+# Time-stamp: <2012-02-16 16:57:24 (ryanc)>
 #
 # Author: Ryan Corder <ryanc@greengrey.org>
 # Description: knc.pl - Eventual OpenBSD netcat clone with Kerberos support
@@ -11,7 +11,9 @@ our $VERSION = '0.010';
 
 use AnyEvent;
 use AnyEvent::Handle;
-use Authen::Krb5 qw(KRB5_NT_SRV_HST AP_OPTS_MUTUAL_REQUIRED);
+use AnyEvent::Log;
+use Authen::Krb5
+    qw(KRB5_NT_SRV_HST AP_OPTS_MUTUAL_REQUIRED ADDRTYPE_INET ADDRTYPE_IPPORT);
 use English qw(-no_match_vars);
 use Getopt::Std;
 use IO::Socket;
@@ -21,14 +23,17 @@ $Getopt::Std::STANDARD_HELP_VERSION = 'true';
 my %opts;
 getopts( 'CkKN:lT:v', \%opts );
 
-# Exit early if certain required things are missing
-die HELP_MESSAGE() if ( !( $ARGV[0] && $ARGV[1] ) );
-die HELP_MESSAGE() if ( $opts{'k'} && ( !$opts{'l'} ) );
+$opts{'v'} && $AnyEvent::Log::FILTER->level('info');
 
-my $host   = $ARGV[0];             # IO::Socket::INET does host/ip validaiton
-my $port   = $ARGV[1];             # IO::Socket::INET does port validation
-my $ready  = AnyEvent->condvar;    # Set up our "main loop"
-my $socket = create_socket();      # Socket needed before Kerberos can setup
+# Exit early if certain required things are missing
+( !( $ARGV[0] && $ARGV[1] ) ) && AE::log fatal => HELP_MESSAGE();
+( $opts{'k'} && ( !$opts{'l'} ) ) && AE::log fatal => HELP_MESSAGE();
+
+my $host    = $ARGV[0];             # IO::Socket::INET does host/ip validaiton
+my $port    = $ARGV[1];             # IO::Socket::INET does port validation
+my $ready   = AnyEvent->condvar;    # Set up our "main loop"
+my $socket  = create_socket();      # Socket needed before Kerberos can setup
+my $krb5_ac = q{none};              # We check later for a valid Auth Context
 
 # Set up Kerberos context
 $opts{'K'} && Authen::Krb5::init_context();
@@ -92,7 +97,7 @@ sub create_socket {
             Listen    => SOMAXCONN,
             Proto     => 'tcp',
             ReuseAddr => 1,
-        ) or die $EVAL_ERROR, "\n";
+        ) or AE::log fatal => "$EVAL_ERROR\n";
     }
 
     # Client mode
@@ -101,7 +106,7 @@ sub create_socket {
             PeerAddr => $host,
             PeerPort => $port,
             Proto    => 'tcp',
-        ) or die $EVAL_ERROR, "\n";
+        ) or AE::log fatal => "$EVAL_ERROR\n";
     }
 
     return $l_socket;
@@ -110,11 +115,30 @@ sub create_socket {
 sub setup_kerberos {
     my $krb5_socket = shift;
 
-    my $krb5_service = defined( $opts{'N'} ) ? $opts{'N'} : 'example';
-    my $krb5_keytab = defined( $opts{'T'} ) ? $opts{'T'} : '/etc/krb5.keytab';
-    my $krb5_ac     = Authen::Krb5::AuthContext->new();
-    my $krb5_spn    = Authen::Krb5::sname_to_principal( $host, $krb5_service,
+    my $krb5_service = $opts{'N'} // q{example};
+    my $krb5_keytab  = $opts{'T'} // q{/etc/krb5.keytab};
+    my $krb5_spn = Authen::Krb5::sname_to_principal( $host, $krb5_service,
         KRB5_NT_SRV_HST );
+
+    $krb5_ac = Authen::Krb5::AuthContext->new();
+
+    # Set local and remote addresses, using network byte order
+    $krb5_ac->setaddrs(
+        Authen::Krb5::Address->new( ADDRTYPE_INET, pack 'a4',
+            $krb5_socket->sockaddr()
+        ),
+        Authen::Krb5::Address->new( ADDRTYPE_INET, pack 'a4',
+            $krb5_socket->peeraddr()
+        )
+    );
+    $krb5_ac->setports(
+        Authen::Krb5::Address->new( ADDRTYPE_INET, pack 'n',
+            $krb5_socket->sockport()
+        ),
+        Authen::Krb5::Address->new( ADDRTYPE_INET, pack 'n',
+            $krb5_socket->peerport()
+        )
+    );
 
     # Server context
     if ( $opts{'l'} ) {
@@ -124,13 +148,12 @@ sub setup_kerberos {
             $krb5_ktr );
 
         if ($krb5_recv) {
-            if ( $opts{'v'} ) {
-                print 'Received kerberos authentication info from '
-                    . $krb5_recv->enc_part2->client->data . "\n";
-            }
+            AE::log info => 'Received Kerberos authentication info from '
+                . $krb5_recv->enc_part2->client->data . "\n";
         }
         else {
-            die 'recvauth error: ' . Authen::Krb5::error() . "\n";
+            AE::log fatal => 'recvauth error: '
+                . Authen::Krb5::error() . "\n";
         }
     }
 
@@ -138,7 +161,20 @@ sub setup_kerberos {
     else {
         my $krb5_cc     = Authen::Krb5::cc_default();
         my $krb5_client = Authen::Krb5::parse_name( $ENV{'USER'} );
-        my $krb5_send   = Authen::Krb5::sendauth(
+
+        # create the replay cache
+        my ( $l_addr, $r_addr ) = $krb5_ac->getaddrs();
+        my $krb5_lap =
+            Authen::Krb5::gen_portaddr( $l_addr, $krb5_socket->sockport() );
+        my $krb5_rcn =
+            Authen::Krb5::gen_replay_name( $krb5_lap,
+            q{knc-} . $krb5_socket->sockport() . q{-} );
+        my $krb5_rc = Authen::Krb5::get_server_rcache($krb5_rcn);
+
+        AE::log info => "Kerberos replay cache ($krb5_rcn) created.\n";
+        $krb5_ac->setrcache($krb5_rc);
+
+        my $krb5_send = Authen::Krb5::sendauth(
             $krb5_ac,  $krb5_socket,
             'V1',      $krb5_client,
             $krb5_spn, AP_OPTS_MUTUAL_REQUIRED,
@@ -147,12 +183,12 @@ sub setup_kerberos {
         );
 
         if ($krb5_send) {
-            if ( $opts{'v'} ) {
-                print "Sent kerberos authentication info for $ENV{'USER'}\n";
-            }
+            AE::log info =>
+                "Sent Kerberos authentication info for $ENV{'USER'}\n";
         }
         else {
-            die 'sendauth error: ' . Authen::Krb5::error() . "\n";
+            AE::log fatal => 'sendauth error: '
+                . Authen::Krb5::error() . "\n";
         }
     }
 
@@ -182,16 +218,88 @@ sub setup_ae_handle {
             }
         },
         on_read => sub {
-            shift->unshift_read(
-                line => sub {
-                    if ( $opts{'C'} ) {
-                        syswrite $fh_out, "$_[1]\r\n";
-                    }
-                    else {
-                        syswrite $fh_out, "$_[1]\n";
-                    }
+            if ( ref($fh_in) =~ /IO::Socket::INET/xms ) {
+                if ( $opts{'K'} ) {
+                    shift->unshift_read(
+                        line => qr{__END\015?\012}xms,    # Custom EOL marker
+                        sub {
+                            if ( $krb5_ac eq 'none' ) {
+                                AE::log fatal =>
+                                    "Kerberos toggled but AC set to 'none'\n";
+                            }
+
+                            my $line =
+                                Authen::Krb5::rd_priv( $krb5_ac, $_[1] );
+
+                            if ( !$line ) {
+                                my $krb5_status = Authen::Krb5::error();
+
+                                # NOT a failure, just an empty message
+                                if ( $krb5_status eq 'Success' ) {
+                                    $line = $line // q{};
+                                }
+                                else {
+                                    AE::log fatal =>
+                                        "Kerberos rd_priv error: $krb5_status\n";
+                                }
+                            }
+
+                            if ( $opts{'C'} ) {
+                                syswrite $fh_out, "$line\r\n";
+                            }
+                            else {
+                                syswrite $fh_out, "$line\n";
+                            }
+                        }
+                    );
                 }
-            );
+                else {
+                    shift->unshift_read(
+                        line => sub {
+                            my $line = $_[1];
+
+                            if ( $opts{'C'} ) {
+                                syswrite $fh_out, "$line\r\n";
+                            }
+                            else {
+                                syswrite $fh_out, "$line\n";
+                            }
+                        }
+                    );
+                }
+            }
+            else {
+                shift->unshift_read(
+                    line => sub {
+                        my $line = $_[1];
+
+                        if ( $opts{'K'} ) {
+                            if ( $krb5_ac eq 'none' ) {
+                                AE::log fatal =>
+                                    "Kerberos toggled but AC set to 'none'\n";
+                            }
+
+                            my $enc_line =
+                                Authen::Krb5::mk_priv( $krb5_ac, $line );
+
+                            if ($enc_line) {
+                                $line = $enc_line . '__END';
+                            }
+                            else {
+                                AE::log fatal => 'Kerberos mk_priv error: '
+                                    . Authen::Krb5::error() . "\n";
+                            }
+                        }
+
+                        if ( $opts{'C'} ) {
+                            syswrite $fh_out, "$line\r\n";
+                        }
+                        else {
+                            syswrite $fh_out, "$line\n";
+                        }
+                    }
+                );
+            }
         }
     ) or AE::log fatal => "Couldn't create AE handle: $OS_ERROR\n";
 
@@ -199,7 +307,7 @@ sub setup_ae_handle {
 }
 
 sub HELP_MESSAGE {
-    print << 'END_HELP';
+    my $help_message = << 'END_HELP';
 
 Usage: knc.pl [-CkKlv] [-N 'service name'] [-T /path/to/krb5.keytab]
        <hostname> <port>
@@ -214,7 +322,8 @@ Syntax:
           current conneciton is completed.  It is an error to use this
           options without the -l option.
 
-    -K    Attempt to perform Kerberos authentication.
+    -K    Attempt to perform Kerberos authentication.  If successful, messages
+          will be encrypted between the client and the server.
 
     -l    Used to specify that knc.pl should listen for an incoming connection
           rather than initiate a connection to a remote host.
@@ -231,6 +340,9 @@ Syntax:
           /etc/krb5.keytab.
 
 END_HELP
+
+    print {*STDERR} $help_message
+        or AE::log fatal => "Printing help to STDERR failed: $OS_ERROR.\n";
 
     return 1;
 }
